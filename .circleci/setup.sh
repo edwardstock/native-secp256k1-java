dec9670e7916ed68a8d054f8a18048780e683bb3
Eduard Maximovich <edward.vstock@gmail.com>
2021-04-15 17:41:48 +0300

    fixed cmake detection

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -2,10 +2,12 @@
 
 mkdir -p /tmp/pkgs
 
-if [ ! -f "/usr/bin/cmake" ] && [ ! -f "/usr/local/bin/cmake" ]; then
+CMAKE_PATH=$(which cmake)
+
+if [ "${CMAKE_PATH}" == "" ]; then
   # fetching cmake
-  CMAKE_MAJOR="3.12"
-  CMAKE_PATCH="4"
+  CMAKE_MAJOR="3.14"
+  CMAKE_PATCH="5"
   CMAKE_VERS="${CMAKE_MAJOR}.${CMAKE_PATCH}"
   if [ ! -f "/tmp/pkgs/cmake.sh" ]; then
     curl -Lso /tmp/pkgs/cmake.sh https://cmake.org/files/v${CMAKE_MAJOR}/cmake-${CMAKE_VERS}-Linux-x86_64.sh
